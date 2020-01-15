a64ccdead8e006638b362c69bcdd0e746b317ad0
Tulio Garcia <tulio@vaadin.com>
2020-01-15 10:43:58 +0200

    Cleanup

--- vaadin-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/timepicker/tests/AbstractTimePickerIT.java
+++ vaadin-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/timepicker/tests/AbstractTimePickerIT.java
@@ -15,43 +15,23 @@ import java.util.List;
 
 public abstract class AbstractTimePickerIT extends AbstractComponentIT {
 
-    private WebDriver createHeadlessChromeDriver() {
-        System.out.println("*** Setup driver");
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-        options.addArguments("--aggressive-cache-discard");
-        options.addArguments("--disable-cache");
-        options.addArguments("--disable-application-cache");
-        options.addArguments("--disable-offline-load-stale-cache");
-        options.addArguments("--disk-cache-size=0");
-        options.addArguments("--headless");
-        options.addArguments("--disable-gpu");
-        options.addArguments("--dns-prefetch-disable");
-        options.addArguments("--no-proxy-server");
-        options.addArguments("--log-level=3");
-        options.addArguments("--silent");
-        options.addArguments("--disable-browser-side-navigation");
-        options.setPageLoadStrategy(PageLoadStrategy.NONE);
-        options.setProxy(null);
-        return TestBench.createDriver(new ChromeDriver(options));
-    }
-
     @Override
     protected List<DesiredCapabilities> getBrowserCapabilities(
         Browser... browsers) {
         return enhanceCapabilities(super.getBrowserCapabilities(browsers));
     }
 
-    static List<DesiredCapabilities> enhanceCapabilities(List<DesiredCapabilities> capabilities) {
-        capabilities.stream().filter(cap -> "chrome".equalsIgnoreCase(cap.getBrowserName())).forEach(AbstractTimePickerIT::enhanceCapabilities);
+    static List<DesiredCapabilities> enhanceCapabilities(
+        List<DesiredCapabilities> capabilities) {
+        capabilities.stream()
+            .filter(cap -> "chrome".equalsIgnoreCase(cap.getBrowserName()))
+            .forEach(AbstractTimePickerIT::enhanceCapabilities);
         return capabilities;
     }
 
     static void enhanceCapabilities(DesiredCapabilities cap) {
-        //cap.setCapability("pageLoadStrategy","none");
         final ChromeOptions options = new ChromeOptions();
         options.addArguments("--headless", "--disable-gpu");
-        cap.setCapability(ChromeOptions.CAPABILITY,options);
+        cap.setCapability(ChromeOptions.CAPABILITY, options);
     }
 }
