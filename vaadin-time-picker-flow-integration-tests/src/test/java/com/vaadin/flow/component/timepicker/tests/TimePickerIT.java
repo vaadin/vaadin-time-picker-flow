a7e90b5b454bc62ce32f31cecbca18a851c1fadf
Kari Söderholm <haprog@vaadin.com>
2020-01-31 15:02:04 +0200

    Convert the remaining ITs to not use the demo view

--- vaadin-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/timepicker/tests/TimePickerIT.java
+++ vaadin-time-picker-flow-integration-tests/src/test/java/com/vaadin/flow/component/timepicker/tests/TimePickerIT.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2000-2017 Vaadin Ltd.
+ * Copyright 2000-2020 Vaadin Ltd.
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may not
  * use this file except in compliance with the License. You may obtain a copy of
@@ -18,54 +18,78 @@ package com.vaadin.flow.component.timepicker.tests;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 
-import com.vaadin.flow.component.timepicker.demo.TimePickerView;
 import com.vaadin.flow.component.timepicker.testbench.TimePickerElement;
-import com.vaadin.flow.demo.ComponentDemoTest;
+import com.vaadin.flow.testutil.AbstractComponentIT;
+import com.vaadin.flow.testutil.TestPath;
+import com.vaadin.testbench.TestBenchElement;
 
 /**
- * Integration tests for the {@link TimePickerView}.
+ * Integration tests for the {@link TimePickerPage}.
  */
-public class TimePickerIT extends ComponentDemoTest {
+@TestPath("time-picker-it")
+public class TimePickerIT extends AbstractComponentIT {
 
     @Before
     public void init() {
+        open();
         $(TimePickerElement.class).waitForFirst();
     }
 
     @Test
     public void selectTimeOnSimpleTimePicker() {
-        WebElement picker = layout.findElement(By.id("simple-picker"));
-        WebElement message = layout.findElement(By.id("simple-picker-message"));
+        TimePickerElement picker = $(TimePickerElement.class)
+                .id("simple-picker");
+        TestBenchElement message = $("div").id("simple-picker-message");
 
-        executeScript("arguments[0].value = '10:08'", picker);
-
+        picker.setValue("10:08");
         waitUntil(driver -> message.getText().contains("Hour: 10\nMinute: 8"));
 
-        executeScript("arguments[0].value = ''", picker);
-
+        picker.setValue("");
         waitUntil(driver -> "No time is selected".equals(message.getText()));
     }
 
     @Test
     public void selectTimeOnDisabledTimePicker() {
-        WebElement picker = layout.findElement(By.id("disabled-picker"));
-        WebElement message = layout
-                .findElement(By.id("disabled-picker-message"));
+        TimePickerElement picker = $(TimePickerElement.class)
+                .id("disabled-picker");
+        TestBenchElement message = $("div").id("disabled-picker-message");
 
-        executeScript("arguments[0].value = '10:15'", picker);
+        picker.setValue("10:15");
         Assert.assertEquals(
                 "The message should not be shown for the disabled picker", "",
                 message.getText());
     }
 
     @Test
+    public void timePickerWithDifferentStep() {
+        TimePickerElement picker = $(TimePickerElement.class)
+                .id("step-setting-picker");
+        picker.openDropDown();
+        Assert.assertEquals("Item in the dropdown is not correct", "1:00 AM",
+                picker.getItemText(1));
+        picker.closeDropDown();
+        executeScript("arguments[0].value = '12:31'", picker);
+
+        selectStep("0.5s");
+        validatePickerValue(picker, "12:31:00.000");
+
+        selectStep("10s");
+        validatePickerValue(picker, "12:31:00");
+
+        // for the auto formatting of the value to work, it needs to match the
+        // new step
+        executeScript("arguments[0].value = '12:30:00'", picker);
+        selectStep("30m"); // using smaller step will cause the drop down to be
+                           // big and then drop down iron list does magic that
+                           // messes the item indexes
+        validatePickerValue(picker, "12:30");
+    }
+
+    @Test
     public void timePickerWithMinAndMaxSetting() {
         TimePickerElement picker = $(TimePickerElement.class)
                 .id("time-picker-min-max");
-        picker.scrollIntoView();
         picker.openDropDown();
         Assert.assertEquals(
                 "The first item in the dropdown should be the min value",
@@ -75,8 +99,14 @@ public class TimePickerIT extends ComponentDemoTest {
                 "6:00 PM", picker.getLastItemText());
     }
 
-    @Override
-    protected String getTestPath() {
-        return ("/vaadin-time-picker");
+    private void selectStep(String step) {
+        NativeSelectElement select = $(NativeSelectElement.class)
+                .id("step-picker");
+        select.setValue(step, true);
+    }
+
+    private void validatePickerValue(TimePickerElement picker, String value) {
+        Assert.assertEquals("Invalid time picker value", value,
+                picker.getValue());
     }
 }
